337353d4e7eec299de161d05b0b4f0bc8aadd14d
amnakhalid123 <144409342+amnakhalid123@users.noreply.github.com>
2025-08-12 22:22:46 +0500

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDLL.Utility
+{
+    internal class Database
+    {
+        public static string connection = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Project;Integrated Security=True";
+
+        public static string GetConnectionString()
+        {
+            return connection;
+        }
+    }
+}
