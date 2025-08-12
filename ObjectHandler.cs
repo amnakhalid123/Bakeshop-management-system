e5c48ab122e980f7067152f0e14da5a6d88b8737
amnakhalid123 <144409342+amnakhalid123@users.noreply.github.com>
2025-08-12 22:20:40 +0500

    Add files via upload

---
+++ ObjectHandler.cs
@@ -0,0 +1,21 @@
+﻿using ProjectDLL.DL.DB;
+using ProjectDLL.DL.FH;
+using ProjectDLL.DLInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    internal class ObjectHandler
+    {
+        private static IProductDL productDL = new ProductFH();
+
+        public static IProductDL GetProductDL()
+        {
+            return productDL;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleProject.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string opt = ProductUI.MainMenu();
+                Console.Clear();
+
+                if (opt == "1")
+                {
+                    Console.WriteLine(ProductUI.ViewProducts());
+                }
+                else if (opt == "2")
+                {
+                    Console.WriteLine(ProductUI.AddProduct());
+                }
+                else if (opt == "3")
+                {
+                    Console.WriteLine(ProductUI.DeleteProduct());
+                }
+                else if (opt == "4")
+                {
+                    Console.WriteLine(ProductUI.UpdatePrice());
+                }
+                else if (opt == "5")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Option!!!");
+                }
+                Console.WriteLine("\nPress Any Key to Continue..");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
